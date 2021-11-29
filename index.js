3a143ec1af2a56f7fa33a12a8e84a1e9dafab92d
Maksym Skreklia <36624127+Skrekliam@users.noreply.github.com>
2021-11-29 11:00:11 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Starting MKR 2 project');
+
+for(let i = 0; i<= 100; i += Math.floor(Math.random() * 15)) {
+    setTimeout(() =>
+    console.log('Loading ....',i,'%')
+    ,1000)
+}
+
+console.log('Loaded')
